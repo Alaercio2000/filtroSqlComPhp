b0f1398687ee3ccda29970cbdfdab41d8565ca60
Alaercio <alaercio.vieira@outlook.com>
2019-11-19 14:23:25 -0300

    Alterando função para evitar erros ao filtra

--- funcoes.php
+++ funcoes.php
@@ -1,25 +1,44 @@
 <?php
 
-function filtroSimples($table , $pdo){
-    $sql = "SELECT * FROM $table";
+function filtroSimples($pdo, $filtro1 = "" , $filtro2 = "" ,$filtro3 = "")
+{
+    $sql = "SELECT
+                o.endereco,
+                b.nome,
+                t.nome,
+                o.duracao_prev,
+                o.duracao_real,
+                o.valor,
+                o.data
+            FROM   
+                (
+                oss o
+                INNER JOIN bairros b ON o.id_bairro=b.id
+                INNER JOIN tipos_de_os t ON o.id_tipo=t.id
+                )
+                WHERE $filtro1 AND $filtro2 AND $filtro3
+                ;
+            ";
+
     $sql = $pdo->query($sql);
     foreach ($sql->fetchAll() as $value) {
         echo ("<tr>");
-        echo("<td scope='col'>". $value[1] ."</td>");
-        echo("<td scope='col'>". $value[2] ."</td>");
-        echo("<td scope='col'>". $value[3] ."</td>");
-        echo("<td scope='col'>". $value[4] ."</td>");
-        echo("<td scope='col'>". $value[5] ."</td>");
-        echo("<td scope='col'>". $value[6] ."</td>");
-        echo("<td scope='col'>". $value[7] ."</td>");
+        echo utf8_encode(("<td scope='col'>" . $value[0] . "</td>"));
+        echo utf8_encode(("<td scope='col'>" . $value[1] . "</td>"));
+        echo utf8_encode(("<td scope='col'>" . $value[2] . "</td>"));
+        echo utf8_encode(("<td scope='col'>" . $value[3] . "</td>"));
+        echo utf8_encode(("<td scope='col'>" . $value[4] . "</td>"));
+        echo utf8_encode(("<td scope='col'>" . $value[5] . "</td>"));
+        echo utf8_encode(("<td scope='col'>" . $value[6] . "</td>"));
         echo ("</tr>");
     }
 }
 
-function trazerDados($table , $nomeCampo , $pdo){
+function trazerDados($table, $nomeCampo, $pdo)
+{
     $sql = "SELECT * FROM $table ;";
     $sql = $pdo->query($sql);
     foreach ($sql->fetchAll() as $value) {
-        echo('<option>'. $value[$nomeCampo].'</option>');
+        echo utf8_encode(("<option value='".$value[$nomeCampo]."'>" . $value[$nomeCampo] . "</option>"));
     }
 }
